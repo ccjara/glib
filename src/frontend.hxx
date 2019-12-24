8bfd424aa1b6a6a40bed8afe1ed224a64af5aaca
Jara <sdp.cpp@gmail.com>
2019-12-24 22:52:51 +0100

    Extract arg_provider

--- src/frontend.hxx
+++ src/frontend.hxx
@@ -8,35 +8,9 @@
 #include <string>
 #include <vector>
 
+#include "arg_provider.hxx"
 #include "exceptions.hxx"
 #include "glib.hxx"
-
-class arg_provider {
-private:
-    std::vector<std::string> args;
-public:
-    arg_provider(int argc, char* argv[]) {
-        if (argc < 0) {
-            throw std::exception();
-        }
-        clear();
-        for (auto i = 0; i < argc; i++) {
-            args.emplace_back(argv[i]);
-        }
-    }
-
-    [[nodiscard]] std::string get(std::vector<std::string>::size_type index) const {
-        const auto max_index { args.size() - 1 };
-        if (index > max_index) {
-            throw too_few_arguments_exception();
-        }
-        return args[index];
-    }
-
-    void clear() {
-        args.clear();
-    }
-};
 
 class frontend {
 private:
--- src/main.cxx
+++ src/main.cxx
@@ -1,3 +1,4 @@
+#include "arg_provider.hxx"
 #include "frontend.hxx"
 
 int main(int argc, char *argv[]) {
